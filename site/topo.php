9813ecae5e8511cce242f9131866cc1b588892c8
Lhaion <Lhaion>
2016-08-25 14:41:35 -0300

    Version 0.4.83

--- site/topo.php
+++ site/topo.php
@@ -22,40 +22,18 @@
 			case "for":
 				$pag = 3;
 				break;
-			case "car":
-				$pag = 4;
-				break;
-			case "cat":
-				$pag = 5;
-				break;
-			case "pro":
-				$pag = 6;
-				break;
-			case "cli":
-				$pag = 7;
-				break;
-			case "fun":
-				$pag = 8;
-				break;
-			case "ite":
-			case "ven":
-				$pag = 9;
-				break;
 		}
 	?>
 	<nav>
 		<ul>
 			<li><a <?= $pag == 1 ? '' : 'href="?pagina=inicio"' ?> class="<?= $pag == 1 ? "active" : '' ?>">Inicio</a></li>
-			<li><a <?= $pag == 2 ? '' : 'href="?pagina=cidades_listar"' ?> class="<?= $pag == 2 ? 'active' : '' ?>">Cidades</a></li>
-			<li><a <?= $pag == 3 ? '' : 'href="?pagina=formas_pagamento_listar"' ?> class="<?= $pag == 3 ? 'active' : '' ?>">Formas Pagamento</a></li>
+			<li><a <?= $pag == 2 ? '' : 'href="?pagina=cidades_listar"' ?> class="<?= $pag == 2 ? 'active' : '' ?>">Produtos</a></li>
+			<li><a <?= $pag == 3 ? '' : 'href="?pagina=formas_pagamento_listar"' ?> class="<?= $pag == 3 ? 'active' : '' ?>">Lojas</a></li>
 
-			<li><a <?= $pag == 4 ? '' : 'href="?pagina=cargos_listar"' ?> class="<?= $pag == 4 ? 'active' : '' ?>">Cargos</a></li>
-			<li><a <?= $pag == 5 ? '' : 'href="?pagina=categorias_listar"' ?> class="<?= $pag == 5 ? 'active' : '' ?>">Categorias</a></li>
-			<li><a <?= $pag == 6 ? '' : 'href="?pagina=produtos_listar"' ?> class="<?= $pag == 6 ? 'active' : '' ?>">Produtos</a></li>
+<!--			<li><a <?/*= $pag == 4 ? '' : 'href="?pagina=cargos_listar"' */?> class="<?/*= $pag == 4 ? 'active' : '' */?>">Cargos</a></li>
+			<li><a <?/*= $pag == 5 ? '' : 'href="?pagina=categorias_listar"' */?> class="<?/*= $pag == 5 ? 'active' : '' */?>">Categorias</a></li>
+			<li><a <?/*= $pag == 6 ? '' : 'href="?pagina=produtos_listar"' */?> class="<?/*= $pag == 6 ? 'active' : '' */?>">Produtos</a></li>-->
 
-			<li><a <?= $pag == 7 ? '' : 'href="?pagina=clientes_listar"' ?> class="<?= $pag == 7 ? 'active' : '' ?>">Clientes</a></li>
-			<li><a <?= $pag == 8 ? '' : 'href="?pagina=funcionarios_listar"' ?> class="<?= $pag == 8 ? 'active' : '' ?>">Funcionários</a></li>
-			<li><a <?= $pag == 9 ? '' : 'href="?pagina=vendas_listar"' ?> class="<?= $pag == 9 ? 'active' : '' ?>">Vendas</a></li>
 		</ul>
 
 	</nav>
